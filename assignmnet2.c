a71d7fe5d0d237ebe906e87a28425891dad6ac7e
Gargee Pomane <impastasouse@gmail.com>
2026-04-20 07:50:17 +0530

    Add files via upload

---
+++ assignmnet2.c
@@ -0,0 +1,29 @@
+//Menu Calculator
+
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float a, b;
+
+    printf("1.Add 2.Subtract 3.Multiply 4.Divide\n");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    switch(choice) {
+        case 1: printf("Result = %.2f", a + b); break;
+        case 2: printf("Result = %.2f", a - b); break;
+        case 3: printf("Result = %.2f", a * b); break;
+        case 4: 
+            if(b != 0)
+                printf("Result = %.2f", a / b);
+            else
+                printf("Division by zero error");
+            break;
+        default: printf("Invalid choice");
+    }
+
+    return 0;
+}
